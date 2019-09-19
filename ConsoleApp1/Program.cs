e45c8bf79e358827c2ebc24893e7696b8fc7a384
y18321782243@163.com <y18321782243@163.com>
2019-09-19 11:28:08 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello world";
+           
+            int ganzeZahl = 16557;
+            double gleitkommaZahl = 168.246;
+            char Zeichen = 'J';
+            bool wahrheitswert = true;
+            //Console.WriteLine(message);
+            //Console.WriteLine(ganzeZahl);
+            //Console.WriteLine(gleitkommaZahl);
+            //Console.WriteLine(wahrheitswert);
+            //Console.WriteLine(Zeichen);
+
+            //for(int i=1; i<=10; i++)
+            //{
+            //    Console.Write(i + " ");
+            //}
+            int zahl;
+            Console.WriteLine("Eine ganze Zahl eingeben:");
+            zahl = Convert.ToInt32(Console.ReadLine()); 
+
+            int zahl1,zahl2
+        }
+    }
+}
